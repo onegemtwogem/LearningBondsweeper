c6bcffb74dcb7fe19469d73139ce5c6d2a9523f1
Unknown <hollyhollystein@gmail.com>
2017-09-01 14:22:53 -0700

    700 line goal

--- #BOND.py
+++ #BOND.py
@@ -7,6 +7,7 @@ import tkMessageBox
 import tkSimpleDialog
 
 # writing notes from github
+# MAKE LESS THAN 700
 
 class MinesweeperCountry(object): 
     """attributes: location, name, neighbors, mine, visibility, adjacent mines
